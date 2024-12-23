0d4cfd32dda92fc9f1231291c4e60182e3f3beb4
Anna Huang <anna.huang011@gmail.com>
2024-12-23 14:34:36 -0800

    testing

--- bootcamp-milestone-2/src/app/api/blogs/[slug]/route.ts
+++ bootcamp-milestone-2/src/app/api/blogs/[slug]/route.ts
@@ -3,10 +3,10 @@ import connectDB from '@/database/db';
 import blogSchema from '@/database/blogSchema';
 
 // The correct function signature for Next.js route handlers
-export async function GET(req: NextRequest, { params }: { params: Promise<{ slug: string }> }) {
+export async function GET(req: NextRequest) {
   await connectDB(); // Connect to the database before querying
 
-  const slug = (await params).slug;
+  const slug = req.nextUrl.pathname.split('/').pop();
 
   try {
     // Try to find the blog post by slug
